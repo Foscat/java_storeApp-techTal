120c6388f6ed7f207c08759965827832309f01bd
Kyle Foster <40616718+Foscat@users.noreply.github.com>
2019-05-11 14:15:20 -0700

    base files

---
+++ storeApp/src/storeApp/Product.java
@@ -0,0 +1,22 @@
+package storeApp;
+
+public class Product {
+	//Every product needs these base properties
+	// name, quantity, serial number cost, sell price, 
+	String name;
+	int quantity;
+	long serialNumber;
+	float cost;
+	float salePrice;
+	
+	//Other features to add
+	// 1) Sell by dates
+	// 2) Have it so manager can see how many items need to sell each week
+	// 3) List it out by product
+	//    -Determine how much
+	//    --each product cost the store to purchase
+	//    --each product brings in revenue
+	//    --each product makes in profit
+	// 4) Make it to item can go on sale and determine the discount level
+
+}
